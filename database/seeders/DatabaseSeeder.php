8c664e64aab37221bfd80e8a87853d832fdcc865
Robert Sarmiento <rg891126@gmail.com>
2025-08-16 17:24:11 -0500

    Seeder de roles agregado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,19 +23,100 @@ class DatabaseSeeder extends Seeder
         $this->call(CustomerDetailSeeder::class);
 
         User::factory()->create([
-            'name' => 'Hugo Sarmiento',
-            'email' => 'huessabe@gmail.com',
-            'password' => Hash::make('sarmi618'),
+            'name' => 'Administrador',
+            'email' => 'admin@nodo.com',
+            'password' => Hash::make('admin@nodo.com'),
             'role_id'=> 1,
             'company_id' => 1,
         ]);
 
         User::factory()->create([
-            'name' => 'Usuario Distribuidor',
-            'email' => 'distribuidor@gmail.com',
-            'password' => Hash::make('sarmi618'),
+            'name' => 'duitama',
+            'email' => 'duitama@nodo.com',
+            'password' => Hash::make('ruta.123'),
             'role_id'=> 2,
             'company_id' => 1,
         ]);
+
+        User::factory()->create([
+            'name' => 'paipa',
+            'email' => 'paipa@nodo.com',
+            'password' => Hash::make('ruta.123'),
+            'role_id'=> 2,
+            'company_id' => 1,
+        ]);
+
+        User::factory()->create([
+            'name' => 'wilinfer',
+            'email' => 'wilinfer@nodo.com',
+            'password' => Hash::make('ruta.123'),
+            'role_id'=> 2,
+            'company_id' => 1,
+        ]);
+
+        User::factory()->create([
+            'name' => 'soata',
+            'email' => 'soata@nodo.com',
+            'password' => Hash::make('ruta.123'),
+            'role_id'=> 2,
+            'company_id' => 1,
+        ]);
+
+        User::factory()->create([
+            'name' => 'sogamoso',
+            'email' => 'sogamoso@nodo.com',
+            'password' => Hash::make('ruta.123'),
+            'role_id'=> 2,
+            'company_id' => 1,
+        ]);
+
+        User::factory()->create([
+            'name' => 'barbosa',
+            'email' => 'barbosa@nodo.com',
+            'password' => Hash::make('rutaapp2025'),
+            'role_id'=> 2,
+            'company_id' => 2,
+        ]);
+
+        User::factory()->create([
+            'name' => 'foranea',
+            'email' => 'foranea@nodo.com',
+            'password' => Hash::make('rutaapp2025'),
+            'role_id'=> 2,
+            'company_id' => 2,
+        ]);
+
+        User::factory()->create([
+            'name' => 'norte',
+            'email' => 'norte@nodo.com',
+            'password' => Hash::make('ruta.123'),
+            'role_id'=> 2,
+            'company_id' => 2,
+        ]);
+
+        User::factory()->create([
+            'name' => 'santander',
+            'email' => 'santander@nodo.com',
+            'password' => Hash::make('ruta.123'),
+            'role_id'=> 2,
+            'company_id' => 2,
+        ]);
+
+        User::factory()->create([
+            'name' => 'sur',
+            'email' => 'sur@nodo.com',
+            'password' => Hash::make('rutaapp2025'),
+            'role_id'=> 2,
+            'company_id' => 2,
+        ]);
+
+        User::factory()->create([
+            'name' => 'tenza',
+            'email' => 'tenza@nodo.com',
+            'password' => Hash::make('ruta.123'),
+            'role_id'=> 2,
+            'company_id' => 2,
+        ]);
     }
 }
+
